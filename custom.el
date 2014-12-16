ce56914c1f95439bcbf89af70827540921ab1772
Tsgzj <wxsun1991@gmail.com>
2014-12-15 19:10:41 -0500

    Changes to be committed:  modified:   custom.el

--- custom.el
+++ custom.el
@@ -15,6 +15,7 @@
 ;;(setq calendar-latitude 39.9)
 ;;(setq calendar-longitude 116.3)
 ;;(setq calendar-location-name "Beijing, China")
+;;(set-language-environment "japanese")
 
 ;;; Time related settings
 ;; show time in 24hours format
@@ -74,10 +75,10 @@ inversion of gas-comment-region"
  '(background-color "#002b36")
  '(background-mode dark)
  '(cursor-color "#839496")
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-blue)))
  '(custom-safe-themes
    (quote
-    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(fci-rule-color "#073642")
  '(foreground-color "#839496")
  '(vc-annotate-background nil)
@@ -125,3 +126,12 @@ inversion of gas-comment-region"
 (exec-path-from-shell-initialize)
 
 (global-set-key (kbd "C-c C-d") 'dash-at-point)
+
+(setq scheme-program-name "/usr/local/bin/scheme")
+(setq geiser-guile-binary "/usr/local/bin/guile")
+(setq geiser-racket-binary "/usr/local/bin/racket")
+(setq geiser-default-implementation 'racket)
+
+
+(require 'window-numbering)
+(window-numbering-mode 1)
